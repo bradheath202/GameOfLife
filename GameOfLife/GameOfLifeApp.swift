ed5c6862f40f0ac189261637bb0a8518e16c6a6d
Brad Heath <bradheath202@gmail.com>
2024-01-20 22:08:24 -0600

    Initial Commit

---
+++ GameOfLife/GameOfLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfLifeApp.swift
+//  GameOfLife
+//
+//  Created by Brad Heath on 1/20/24.
+//
+
+import SwiftUI
+
+@main
+struct GameOfLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
